de52ddb187c5c6e1c6daae024bd1b648a1343c93
VasilisMos <64889653+VasilisMos@users.noreply.github.com>
2021-11-15 02:01:13 +0200

    add parallel version for get_scholar_names

--- python_files/google_scholar_main.py
+++ python_files/google_scholar_main.py
@@ -9,26 +9,41 @@ Copyright (c) 2021 Your Company
 from csd_csv_parser import *
 from google_search import *
 from google_scholar_scraper import *
-import time
 import pandas as pd
-import my_time as mt
-from scholarly import scholarly, ProxyGenerator
+
+
+def get_names(chunk, res_list):
+    for item in chunk:
+        new_author = get_scholar_name(item)
+        res_list.append(new_author)
+        
+
+def google_scholar_names_parser(dict_dat, thread_num):
+    if thread_num > len(dict_dat): thread_num = len(dict_dat)
+    chunk_list = chunks(dict_dat, threads=thread_num)
+    threads = []
+    result_list = []
+
+    for chunk in chunk_list:
+        x = threading.Thread(target=get_names, args=(chunk, result_list))
+        threads.append(x)
+        x.start()
+
+    for thread in threads:
+        thread.join()
+
+    return result_list
+
 
 csd_in = pd.read_excel(r"..\csv_files\csd_data_in.xlsx")
 csd_out = pd.read_excel(r"..\csv_files\csd_data_out.xlsx")
 
 # preprocessing
-# csd_in
-csd_in = df_to_dict_parser(csd_in)
-csd_out = df_to_dict_parser(csd_out)
+csd_in_dict = df_to_dict_parser(csd_in)
+csd_out_dict = df_to_dict_parser(csd_out)
 
-# find author name in google scholar
-for i in range(5): # range(len(csd_out))
-    time.sleep(1)
-    get_scholar_name(csd_out[i])
+csd_in_dict_new = google_scholar_names_parser(csd_in_dict, thread_num=5)
 
 # save in csv
-df = pd.DataFrame.from_records(csd_out)
-df.to_csv(path_or_buf=r'..\csv_files\csd_data_out_processed.csv', index=False)
-    
-
+df = pd.DataFrame.from_records(csd_in_dict_new)
+df.to_csv(path_or_buf=r'..\csv_files\csd_data_in_processed.csv', index=False)
