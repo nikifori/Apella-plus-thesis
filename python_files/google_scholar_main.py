fc36831d4161a8bcbc0f2bf748dff38e460333cc
nikifori <nikfkost@gmail.com>
2021-11-17 17:33:18 +0200

    fix Pitas and Plataniotis

--- python_files/google_scholar_main.py
+++ python_files/google_scholar_main.py
@@ -42,13 +42,32 @@ csd_in_list_dict = pd.read_csv(r"..\csv_files\csd_data_in_processed_ground_truth
 csd_out_list_dict = pd.read_csv(r"..\csv_files\csd_data_out_processed_ground_truth.csv").to_dict(orient="records")
 
 
-for professor in csd_out_list_dict:
-    try:
-        pg = ProxyGenerator()
-        success = pg.SingleProxy(http = "http://kartzafos22:1gnsjksaDs6FkTGT@proxy.packetstream.io:31112")
-        scholarly.use_proxy(pg)
-        paper_scraper(professor, threads_num=25, host="localhost:27017", json_file_path=r"..\json_files")
-    except Exception as error:
-        print("There is a problem in paper_scraper")
-        print(error)
+# for professor in csd_out_list_dict:
+#     try:
+#         pg = ProxyGenerator()
+#         success = pg.SingleProxy(http = "http://kartzafos22:1gnsjksaDs6FkTGT@proxy.packetstream.io:31112")
+#         scholarly.use_proxy(pg)
+#         paper_scraper(professor, threads_num=25, host="localhost:27017", json_file_path=r"..\json_files")
+#     except Exception as error:
+#         print("There is a problem in paper_scraper")
+#         print(error)
+
+# open saved json file as dictionary
+# with open(r'..\json_files\csd_in_with_abstract\csd_in_with_abstracs_db.json', encoding="utf8") as json_file:
+#     csd_out_list_dict = json.load(json_file)
+
+# save dictionary as json file
+# json_file = json.dumps(author, indent=4)
+# json_name = author["name"].replace(" ", "_").replace(r"/", "_")
+# with open(fr'..\json_files\{json_name}.json', 'w', encoding='utf-8') as f:
+#     f.write(f"[{json_file}]")
+
+
+
+
+
+
+
+
+
     
