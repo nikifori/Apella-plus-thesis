1704d936f57b6b601a212dc9de25d080d0b0ed49
nikifori <nikfkost@gmail.com>
2021-12-19 02:18:50 +0200

    apella_data as json file

--- embeddings_py/utils.py
+++ embeddings_py/utils.py
@@ -9,15 +9,17 @@ Copyright (c) 2021 Your Company
 import json
 
 def save2json(json_fi: list, path2save: str):
-    json_file = json.dumps(json_fi, indent=4)
+    json_file = json.dumps(json_fi, indent=4, ensure_ascii=False)
     with open(fr'{path2save}', 'w', encoding='utf-8') as f:
         f.write(json_file)
             
 def open_json(path2read: str):
-    with open(fr'{path2read}', encoding="utf8") as json_file:
+    with open(fr'{path2read}', encoding="utf-8") as json_file:
         dictionary = json.load(json_file)
     return dictionary
 
 if __name__ == '__main__':
     
-    test = open_json("..\json_files\can_not_fetch_complete.json")
+    csd_out_completed_missing_2 = open_json(r"E:\GitHub_clones\Apella-plus-thesis\json_files\csd_out_with_abstract\csd_out_completed_missing_2.json")
+    save2json(csd_out_completed_missing_2, path2save=r"E:\GitHub_clones\Apella-plus-thesis\json_files\csd_out_with_abstract\csd_out_completed_missing_22.json")
+    test = open_json(r'.\specter_rankings\test_apella_data.json')
