0d0dbd42826c6f7d16edfa3098eedd40d6d9a4d6
VasilisMos <64889653+VasilisMos@users.noreply.github.com>
2022-02-04 12:07:20 +0200

    Update scibert_embeddings.py

--- embeddings_py/scibert_embeddings.py
+++ embeddings_py/scibert_embeddings.py
@@ -1,27 +1,17 @@
-import torch
 import numpy as np
-from metrics import *
-
 from itertools import islice
 import ijson
-import re
 
-from embeddings.emb_clustering import embeddings_clustering
-from embeddings.metrics import find_author_relevance, average_precision
-from embeddings.utils import find_author_rank, open_json, mkdirs, get_positions
-from sentence_transformers import util
-from embeddings.sentence_embeddings2 import calculate_similarities, get_scibert_model, get_embedding
-
-
-def roman_name_to_fname(roman_name:str, in_or_out, aggregation_type="average"):
-    auth_underscore_name = re.sub('/', '_', roman_name)
-    auth_underscore_name = re.sub(' ', '_', auth_underscore_name)
-    fname_emb = fr"./author_embeddings/scibert_embeddings/{aggregation_type}/{in_or_out}/{auth_underscore_name}.csv"
-    return fname_emb
+from metrics import *
+from embeddings.sentence_transformer_models import get_embedding, get_scibert_model, create_author_embeddings
+from embeddings.metrics import find_author_relevance
+from embeddings.utils import find_author_rank, get_underscored_name, mkdirs, get_positions
+from embeddings.sentence_embeddings2 import calculate_similarities
 
 
 def rank_candidates(fname, in_or_out, title, description, position_rank, mode, clustering_params:dict):
     model, tokenizer = get_scibert_model()
+    model_type = 'scibert_average'
     title_embedding = get_embedding(title + tokenizer.sep_token + description, model, tokenizer)
 
     similarity_score = []
@@ -32,12 +22,13 @@ def rank_candidates(fname, in_or_out, title, description, position_rank, mode, c
         objects = islice(objects, 500)
         for i, author in enumerate(objects):
             print(f"{i}.",author['romanize name'])
+            auth_underscore_name = get_underscored_name(author['romanize name'])
             author_rank = find_author_rank(author)
             sim_val = 0.0
 
             if author_rank <= position_rank:
                 try:
-                    fname_emb = roman_name_to_fname(author["romanize name"], in_or_out, aggregation_type="average")
+                    fname_emb = fr"./author_embeddings/{model_type}_embeddings/{in_or_out}/{auth_underscore_name}.csv"
                     author_embeddings = np.genfromtxt(fname_emb, delimiter=',')
                     sim_val = calculate_similarities(title_embedding, author_embeddings, mode, clustering_params, success=True)
                 except:
@@ -76,76 +67,38 @@ def main_ranking_authors(fname, in_or_out, titles, descriptions, authors_targets
         res_target.to_csv('{}_target.csv'.format(fname_output), encoding='utf-8', index=False)
 
 
-def create_scibert_embeddings(fname: str):
-
-    model, tokenizer = get_scibert_model()
-    mkdirs("./author_embeddings/scibert_embeddings/average")
-
-    with open(fname, encoding='utf-8') as f:
-        authors = ijson.items(f, 'item')
-        authors = islice(authors, 500)
-        for i, author in enumerate(authors):
-            print(f"{i}.",author['romanize name'])
-            auth_underscore_name = re.sub('/', '_', author['romanize name'])
-            auth_underscore_name = re.sub(' ', '_', auth_underscore_name)
-            if "Publications" not in author:
-                open(fr"./author_embeddings/scibert_embeddings/cls/{auth_underscore_name}.csv", 'w')
-                open(fr"./author_embeddings/scibert_embeddings/average/{auth_underscore_name}.csv", 'w')
-                continue
-
-            cls_embeddings = []
-            average_embeddings = []
-            for pub in author["Publications"]:
-                try:
-                    text = pub['Title'] + ". " + pub["Abstract"]
-                except:
-                    text = pub['Title']
-                model_input = tokenizer(text, padding=True, truncation=True, return_tensors="pt", max_length=512)
-                result = model(**model_input)
-                embeddings = result.last_hidden_state[0, :, :].detach().cpu().numpy()
-
-                cls_embeddings.append(embeddings[0, :])
-                average_embeddings.append(np.mean(embeddings, axis=0))
-            pd.DataFrame(np.matrix(np.matrix(cls_embeddings))).to_csv(fr"./author_embeddings/scibert_embeddings/cls/{auth_underscore_name}.csv", header=False,index=False)
-            pd.DataFrame(np.matrix(np.matrix(average_embeddings))).to_csv(fr"./author_embeddings/scibert_embeddings/average/{auth_underscore_name}.csv", header=False,index=False)
-
-
-def create_author_aggregations(author, in_or_out="in", aggregation_type="average"):
-    papers = author['Publications'] if "Publications" in author else []
-    print("Author:{}, total papers:{}".format(author['name'], len(papers)))
-    auth_underscore_name = re.sub('/', '_', author["romanize name"])
-    auth_underscore_name = re.sub(' ', '_', auth_underscore_name)
-    author_embeddings = []
-
-    fname_emb = roman_name_to_fname(author["romanize name"], in_or_out, aggregation_type=aggregation_type)
-    author_embeddings = np.genfromtxt(fname_emb, delimiter=',')
-    print(author_embeddings.shape)
-    author_embeddings = torch.tensor(author_embeddings)
-
-    n_clusters = [2, 3, 4, 5, 7]
-    dims_reduced = [2, 3, 5, 7, 10]
-    clustering_types = ['agglomerative', 'kmeans']
-    reduction_types = ['PCA', 'isomap']
-
-    mkdirs(f'./author_embeddings/scibert_embeddings/aggregations/{aggregation_type}/{in_or_out}/{auth_underscore_name}')
-    fname_base = f'./author_embeddings/scibert_embeddings/aggregations/{aggregation_type}/{in_or_out}/{auth_underscore_name}/'
-
-    for clustering_type in clustering_types:
-        for reduction_type in reduction_types:
-            for n_cluster in n_clusters:
-                for d in dims_reduced:
-                    try:
-                        # print("{}-{}-{}-{}".format(author['romanize name'], clustering_type, n_cluster, reduction_type, d))
-                        author_centroids = embeddings_clustering(author_embeddings,clustering_type,reduction_type,n_cluster,dimensions_reduced=d)
-                        fname_out = fname_base + "{}_{}_{}_{}.csv".format(clustering_type, n_cluster, reduction_type, d)
-                        pd.DataFrame(np.matrix(author_centroids)).to_csv(fname_out, header=False, index=False)
-                    except Exception as e:
-                        print("Problem for author:{}, {}-{}-{}-{}".format(author['romanize name'], clustering_type, n_cluster, reduction_type, d))
-                        print(e)
-
-    fname_out = fname_base + "mean.csv"
-    pd.DataFrame(np.matrix(torch.mean(author_embeddings, dim=0))).to_csv(fname_out, header=False, index=False)
-
+# def create_scibert_embeddings(fname: str):
+#
+#     model, tokenizer = get_scibert_model()
+#     mkdirs("./author_embeddings/scibert_embeddings/average")
+#
+#     with open(fname, encoding='utf-8') as f:
+#         authors = ijson.items(f, 'item')
+#         authors = islice(authors, 500)
+#         for i, author in enumerate(authors):
+#             print(f"{i}.",author['romanize name'])
+#             auth_underscore_name = re.sub('/', '_', author['romanize name'])
+#             auth_underscore_name = re.sub(' ', '_', auth_underscore_name)
+#             if "Publications" not in author:
+#                 open(fr"./author_embeddings/scibert_embeddings/cls/{auth_underscore_name}.csv", 'w')
+#                 open(fr"./author_embeddings/scibert_embeddings/average/{auth_underscore_name}.csv", 'w')
+#                 continue
+#
+#             cls_embeddings = []
+#             average_embeddings = []
+#             for pub in author["Publications"]:
+#                 try:
+#                     text = pub['Title'] + ". " + pub["Abstract"]
+#                 except:
+#                     text = pub['Title']
+#                 model_input = tokenizer(text, padding=True, truncation=True, return_tensors="pt", max_length=512)
+#                 result = model(**model_input)
+#                 embeddings = result.last_hidden_state[0, :, :].detach().cpu().numpy()
+#
+#                 cls_embeddings.append(embeddings[0, :])
+#                 average_embeddings.append(np.mean(embeddings, axis=0))
+#             pd.DataFrame(np.matrix(np.matrix(cls_embeddings))).to_csv(fr"./author_embeddings/scibert_embeddings/cls/{auth_underscore_name}.csv", header=False,index=False)
+#             pd.DataFrame(np.matrix(np.matrix(average_embeddings))).to_csv(fr"./author_embeddings/scibert_embeddings/average/{auth_underscore_name}.csv", header=False,index=False)
 
 
 if __name__ == '__main__':
