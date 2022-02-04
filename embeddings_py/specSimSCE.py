b5132156d7d09cf9bdf13e6c53d7dc9b4db0943f
nikifori <nikfkost@gmail.com>
2022-02-05 00:37:29 +0200

    add sentence dataset

---
+++ embeddings_py/specSimSCE.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@File    :   specSimSCE.py
+@Date    :   Fri Feb  4 22:34:07 2022
+@Author  :   nikifori 
+@Contact :   nikfkost@gmail.com
+"""
+from utils import*
+from transformers import AutoTokenizer, AutoModel
+from sentence_transformer_models import get_specteget_specter_model
+
+
+if __name__ == "__main__":
+    pass
+    model, tokenizer = get_specteget_specter_model()
